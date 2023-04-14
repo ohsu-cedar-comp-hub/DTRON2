820f29cdf7e39591e5b112228af213822ce0fb70
metincansiper <metincansiper@gmail.com>
2023-04-13 18:51:45 -0700

    Defined prompt_save_dialog() utility function and used it as needed

---
+++ scripts/Annotation/util/ui.py
@@ -0,0 +1,24 @@
+from qtpy.QtWidgets import QMessageBox
+
+def prompt_save_dialog(prompt_text, on_save, on_cancel):
+    # if export_handler and export_handler.is_updated():
+    reply = QMessageBox.question(
+        None,
+        'Save',
+        prompt_text,
+        QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel
+    )
+
+    if reply == QMessageBox.Cancel:
+        # Cancel the ComboBox selection change
+        if on_cancel:
+            on_cancel()
+        # cancel option is selected return false to indicate that
+        return False
+
+    if reply == QMessageBox.Save:
+        # Save changes
+        on_save()
+
+    # cancel option is not selected return true to indicate that
+    return True
